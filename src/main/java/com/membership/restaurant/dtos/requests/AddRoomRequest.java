063ee7797282e5a7e5f6929a2fc0777bdc74434d
Vincent Tsang <vincent_tsang2017@outlook.com>
2023-08-23 14:17:22 +0800

    1.3.3 - RELEASE Fixed bugs in AddRoomRequest.java

--- src/main/java/com/membership/restaurant/dtos/requests/AddRoomRequest.java
+++ src/main/java/com/membership/restaurant/dtos/requests/AddRoomRequest.java
@@ -1,5 +1,6 @@
 package com.membership.restaurant.dtos.requests;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -7,19 +8,32 @@ import java.math.BigDecimal;
 
 @Data
 public class AddRoomRequest {
+    @JsonProperty(value = "booker_num")
     private int bookerNum;
+
     @NotNull
+    @JsonProperty(value = "room_detail")
     private String roomDetail;
+
     @NotNull
+    @JsonProperty(value = "room_image")
     private String roomImage;
+
     @NotNull
+    @JsonProperty(value = "room_intro")
     private String roomIntro;
+
     @NotNull
+    @JsonProperty(value = "room_name")
     private String roomName;
 
+    @JsonProperty(value = "room_num")
     private int roomNum;
 
+    @JsonProperty(value = "room_price")
     private BigDecimal roomPrice;
+
     @NotNull
+    @JsonProperty(value = "room_type")
     private String roomType;
 }
